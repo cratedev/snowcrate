3ccd4a2bba2b479c0533633049a7ad2c87686472
Matthew Henderson <matt@crate.dev>
2025-06-02 20:12:06 -0400

    fake fullscreen script

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -48,7 +48,7 @@ in {
 
         exec-once = [
           "swww-daemon"
-          #          "swww img /home/matt/nix-config/wallpaper/12.png"
+          "swww img /home/matt/snowcrate/assets/wallpaper/12.png"
           "waybar"
           #          "1password --ozone-platform-hint=auto"
           #          "nm-applet"
@@ -73,10 +73,39 @@ in {
           border_size = 1;
         };
 
+        workspace = [
+          "w[t1], gapsout:10, gapsin:10"
+          "w[tg1], gapsout:10, gapsin:10"
+          "f[1], gapsout:10, gapsin:10"
+        ];
+
+        windowrulev2 = [
+          "bordersize 0, floating:0, onworkspace:w[t1]"
+          "rounding 0, floating:0, onworkspace:w[t1]"
+          "bordersize 0, floating:0, onworkspace:w[tg1]"
+          "rounding 0, floating:0, onworkspace:w[tg1]"
+          "bordersize 0, floating:0, onworkspace:f[1]"
+          "rounding 0, floating:0, onworkspace:f[1]"
+        ];
+
         animations = {
-          enabled = true;
-          bezier = "easeOutExpo, 0.19, 1.0, 0.22, 1.0";
-          animation = "windows, 1, 7, easeOutExpo";
+          enabled = "yes";
+
+          # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+          bezier = [
+            "easein, 0.47, 0, 0.745, 0.715"
+            "myBezier, 0.05, 0.9, 0.1, 1.05"
+            "overshot, 0.13, 0.99, 0.29, 1.02"
+            "scurve, 0.98, 0.01, 0.02, 0.98"
+          ];
+
+          animation = [
+            "border, 1, 10, default"
+            "fade, 1, 10, default"
+            "windows, 1, 5, overshot, popin 10%"
+            "windowsOut, 1, 7, default, popin 10%"
+            "workspaces, 1, 6, overshot, slide"
+          ];
         };
 
         decoration = {
@@ -91,6 +120,7 @@ in {
             "$mod, Q, killactive"
             "$mod, V, togglefloating"
             "$mod, F, fullscreen"
+            "$mod, S, exec, ./scripts/fake_fullscreen.sh"
             "$mod, SPACE, exec, rofi -show run"
             "$mod, P, pseudo"
             "$mod, left, movefocus, l"
