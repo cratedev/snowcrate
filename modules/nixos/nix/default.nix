6d534ea3d836a1b60393c3c4803c5c57ad184d7d
Matthew Henderson <matt@crate.dev>
2025-08-27 01:28:43 -0400

    nix-repl
    removed nix-repl

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -59,7 +59,7 @@ in {
       inherit (cfg) package;
 
       settings = {
-        experimental-features = "nix-command flakes repl-flake";
+        experimental-features = "nix-command flakes";
         http-connections = 50;
         warn-dirty = false;
         log-lines = 50;
