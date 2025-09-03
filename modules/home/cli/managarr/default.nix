ca9e951b7d00b5dfbd291e571467add57ced13cd
Matthew Henderson <matt@crate.dev>
2025-09-02 21:00:01 -0400

    managarr
    added sonarr

--- modules/home/cli/managarr/default.nix
+++ modules/home/cli/managarr/default.nix
@@ -18,6 +18,10 @@ in {
       - host: 10.0.0.10
         port: 7878
         api_token: 09b8b6d99bfa4bb89a54d55c1e750381
+      sonarr:
+      - host: 10.0.0.10
+        port: 8989
+        api_token: 49518ebb0488401d9f5a9d675ee8dc37
     '';
   };
 }
