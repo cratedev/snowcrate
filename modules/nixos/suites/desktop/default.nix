6be4793856c36e7aa61925bc5bcbd0e9fb3e028e
Matthew Henderson <matt@crate.dev>
2025-10-16 22:16:57 -0400

    remove repo setup and secrets

--- modules/nixos/suites/desktop/default.nix
+++ modules/nixos/suites/desktop/default.nix
@@ -25,30 +25,5 @@ in {
         steam = enabled;
       };
     };
-    age = {
-      secrets = {
-        id_ed25519 = {
-          file = "${inputs.mysecrets}/secrets/desktop/id_ed25519.age";
-          path = "/home/matt/.ssh/id_ed25519";
-          owner = "matt";
-          group = "users";
-          mode = "600";
-        };
-        id_ed25519_pub = {
-          file = "${inputs.mysecrets}/secrets/desktop/id_ed25519_pub.age";
-          path = "/home/matt/.ssh/id_ed25519.pub";
-          owner = "matt";
-          group = "users";
-          mode = "644";
-        };
-      };
-      identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-    };
-
-    users.users.matt = {
-      isNormalUser = true;
-      home = "/home/matt";
-      extraGroups = ["users"];
-    };
   };
 }
