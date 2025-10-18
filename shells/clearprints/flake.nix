3f112e6626b340fb8250fb65b2b1e2a2f85412b0
Matthew Henderson <matt@crate.dev>
2025-10-18 01:54:38 -0400

    devShell

--- shells/clearprints/flake.nix
+++ shells/clearprints/flake.nix
@@ -1,37 +1,24 @@
 {
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # Stable Nixpkgs
-  outputs = {self, ...} @ inputs: let
-    supportedSystems = [
-      "x86_64-linux"
-    ];
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
 
-    forEachSupportedSystem = f:
-      inputs.nixpkgs.lib.genAttrs supportedSystems (
-        system:
-          f {
-            pkgs = import inputs.nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          }
-      );
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
   in {
-    devShells = forEachSupportedSystem (
-      {pkgs}: {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            python3
-            python3Packages.pygobject3
-            gobject-introspection
-            libfprint
-            gusb
-            libfprint-2-tod1-goodix
-          ];
-          shellHook = ''
-            echo "Run 'sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH PYTHONPATH=$PYTHONPATH GI_TYPELIB_PATH=$GI_TYPELIB_PATH python3 ./fprint_clear_storage.py'"
-          '';
-        };
-      }
-    );
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        python3
+        python3Packages.pygobject3
+        gobject-introspection
+        libfprint
+        gusb
+        libfprint-2-tod1-goodix
+      ];
+    };
   };
 }
