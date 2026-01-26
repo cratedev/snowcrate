fcb79b1cddcbdd5162a9063e2e4ac5b600e2fa9c
Matthew Henderson <matt@crate.dev>
2026-01-25 19:49:31 -0500

    Cleanup
    Removed managarr, removed redundant server configs

--- homes/x86_64-linux/matt@crate-server/default.nix
+++ homes/x86_64-linux/matt@crate-server/default.nix
@@ -11,28 +11,8 @@ with lib.${namespace}; {
   };
 
   config = mkIf config.${namespace}.profiles.server.enable {
-    ${namespace} = {
-      user.enable = true;
-      xdg.enable = true;
-
-      cli = {
-        nushell.enable = true;
-        fish.enable = true;
-        btop.enable = true;
-        zellij.enable = true;
-        fzf.enable = true;
-        just.enable = true;
-      };
-
-      tools = {
-        nvf.enable = true;
-        git.enable = true;
-      };
-    };
+    ${namespace}.profiles.server.enable = true;
   };
-
-  # Just enable the server profile
-  ${namespace}.profiles.server.enable = true;
 
   home.stateVersion = "24.05";
 }
