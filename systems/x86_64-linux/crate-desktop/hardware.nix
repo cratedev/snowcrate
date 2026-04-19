27a62b9efc56fb3de539ecd8052bcc682a304c25
Matthew Henderson <matt@crate.dev>
2026-04-19 16:37:16 -0400

    impermanence fix

--- systems/x86_64-linux/crate-desktop/hardware.nix
+++ systems/x86_64-linux/crate-desktop/hardware.nix
@@ -20,27 +20,38 @@
       availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
       kernelModules = [];
 
-      postResumeCommands = lib.mkAfter ''
-        mkdir -p /btrfs_tmp
-        mount -o subvol=/ /dev/disk/by-uuid/84be45b9-f3a6-483c-b26d-201456283506 /btrfs_tmp
+      systemd = {
+        enable = true;
+        services.rollback = {
+          description = "Rollback BTRFS subvolumes";
+          wantedBy = ["initrd.target"];
+          after = ["initrd-root-device.target"];
+          before = ["sysroot.mount"];
+          unitConfig.DefaultDependencies = "no";
+          serviceConfig.Type = "oneshot";
+          script = ''
+            mkdir -p /btrfs_tmp
+            mount -o subvol=/ /dev/disk/by-uuid/84be45b9-f3a6-483c-b26d-201456283506 /btrfs_tmp
 
-        delete_subvolume_recursively() {
-        	IFS=$'\n'
-        	for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-        		delete_subvolume_recursively "/btrfs_tmp/$i"
-        	done
-        	btrfs subvolume delete "$1"
-        }
+            delete_subvolume_recursively() {
+            	IFS=$'\n'
+            	for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            		delete_subvolume_recursively "/btrfs_tmp/$i"
+            	done
+            	btrfs subvolume delete "$1"
+            }
 
-        [[ -e /btrfs_tmp/@root ]] && delete_subvolume_recursively /btrfs_tmp/@root
-        [[ -e /btrfs_tmp/@home ]] && delete_subvolume_recursively /btrfs_tmp/@home
-        [[ -e /btrfs_tmp/@var ]] && delete_subvolume_recursively /btrfs_tmp/@var
+            [[ -e /btrfs_tmp/@root ]] && delete_subvolume_recursively /btrfs_tmp/@root
+            [[ -e /btrfs_tmp/@home ]] && delete_subvolume_recursively /btrfs_tmp/@home
+            [[ -e /btrfs_tmp/@var ]] && delete_subvolume_recursively /btrfs_tmp/@var
 
-        btrfs subvolume create /btrfs_tmp/@root
-        btrfs subvolume create /btrfs_tmp/@home
-        btrfs subvolume create /btrfs_tmp/@var
-        umount /btrfs_tmp
-      '';
+            btrfs subvolume create /btrfs_tmp/@root
+            btrfs subvolume create /btrfs_tmp/@home
+            btrfs subvolume create /btrfs_tmp/@var
+            umount /btrfs_tmp
+          '';
+        };
+      };
     };
   };
 
